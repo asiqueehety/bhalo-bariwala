5c0fb3b82708b15dec7b66b59027e7e56e6ec465
Asique Ehetasamul Haque <82958963+asiqueehety@users.noreply.github.com>
2025-10-03 19:20:33 +0600

    mainActivity change for conflict resolve

--- app/src/main/java/com/example/bhalobariwala/MainActivity.java
+++ app/src/main/java/com/example/bhalobariwala/MainActivity.java
@@ -2,14 +2,45 @@ package com.example.bhalobariwala;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin, btnSignup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // No setContentView() so we don't need activity_main.xml
-        startActivity(new Intent(this, com.example.bhalobariwala.ui.login.LoginActivity.class));
-        finish();
+
+        // Enable EdgeToEdge for modern UI
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        // Apply system window insets (status bar, navigation bar)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        //Buttons
+        btnLogin = findViewById(R.id.btnLogin);
+        btnSignup = findViewById(R.id.btnSignup);
+
+        btnLogin.setOnClickListener(v -> {
+            // Start LoginActivity (make sure you create it)
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        });
+
+        btnSignup.setOnClickListener(v -> {
+            // Start SignupActivity (make sure you create it)
+            startActivity(new Intent(MainActivity.this, SignupActivity.class));
+        });
     }
 }
